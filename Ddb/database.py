d36c01dcbaa754f856cd244c483c92aff42afc31
penguindustin <penguindustin@gmail.com>
2019-09-11 21:43:20 -0700

    Add tables (#1)
    * Added
    
    - table creation, insertion, and all functionality
    - `StorageDriver` class to read and write data to database file
    - tests for functionality
    - README

--- Ddb/database.py
+++ Ddb/database.py
@@ -1,9 +1,39 @@
+import os
+import json
+from typing import List
 
-class Datastore(object):
+class StorageDriver(object):
     """ Data storage for database operations. Basically a wrapper around json
     """
-    def __init__(self, file_name):
-        pass
+
+    def __init__(self, file_name: str):
+        self._file_handler = open(file_name, "w+")
+
+    def _read(self) -> dict:
+        """ Return rows from the database, if empty, return None
+        """
+        # Go to file end to get size
+        self._file_handler.seek(0, os.SEEK_END)
+        size = self._file_handler.tell()
+
+        if size == 0:
+            return None
+
+        self._file_handler.seek(0)
+        data = json.load(self._file_handler)
+        return data
+
+    def _write(self, data: dict):
+        """ Write dict data into our datafile
+        """
+        self._file_handler.seek(0)
+        self._file_handler.write(json.dumps(data, indent=4))
+        self._file_handler.flush()
+
+        # force file to update and trim excess
+        os.fsync(self._file_handler.fileno())
+        self._file_handler.truncate()
+
 
 class Database(object):
     def __init__(self, datafile: str):
@@ -12,10 +42,43 @@ class Database(object):
 
             :param datafile: name of the location to store the data ex: 'data.json'
         """
-        self._storage = Datastore(datafile)
-    
-    def table(self, table_name:str ):
+        self._storage = StorageDriver(datafile)
+
+    def table(self, table_name: str):
         """
             Creates a table if it doesn't exist and return it to the user.
         """
-        pass
+        table = Table(table_name, self._storage)
+        return table
+
+
+class Table(object):
+    """ Handles all the read and write operations to the storage driver
+    """
+
+    def __init__(self, table_name: str, storage: StorageDriver):
+        """ Creates the table
+        """
+        self._name = table_name
+        self._storage = storage
+
+    def insert(self, data: dict):
+        """ insert data into our storage driver and only affect THIS table
+        """
+        table = self._storage._read()
+        # Handles empty file case
+        if table == None:
+            table = {self._name: {"rows": []}}
+        # Handles first usage of table case
+        if self._name not in table:
+            table[self._name] = {"rows": []}
+        table[self._name]["rows"].append(data)
+        self._storage._write(table)
+
+    def all(self) -> List[dict]:
+        """ Get all documents stored in table.
+
+            :returns: a list of documents
+            :rtype: list[dict]
+        """
+        return self._storage._read()[self._name]['rows']
--- tests/test_ddb.py
+++ tests/test_ddb.py
@@ -4,11 +4,27 @@ import Ddb
 def test_database_creation():
     db = Ddb.Database("data.json")
 
-def test_table_creation():
+def test_table_creation_and_insertion():
     db = Ddb.Database("data.json")
     table = db.table('Words')
+    table.insert({"Hello" : "World"})
+    table.insert({"Up" : "Down"})
 
+def test_multiple_table_creation():
+    db = Ddb.Database("data.json")
+    table = db.table('MyTable')
+    table.insert({"key" : "value"})
+    table2 = db.table('OtherTable')
+    table2.insert({"key" : "value"})
 
-# def test_hello():
-#     Database = Ddb.Database()
-#     print("hello world!")
+def test_all():
+    db = Ddb.Database("data.json")
+    table = db.table('Words')
+    documents = [
+        {"Hello" : "World"},
+        {"Up" : "Down"},
+        {"Left" : "Right"},
+    ]
+    for doc in documents:
+        table.insert(doc)
+    assert documents == table.all()
