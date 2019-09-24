f9c5c37daa1fac512247725495bfb04d87849e68
Dustin Mendoza <penguindustin@gmail.com>
2019-09-23 21:41:25 -0700

    added create_query helper function

--- Ddb/queries.py
+++ Ddb/queries.py
@@ -10,30 +10,46 @@ class Query(object):
         query = type(self)()
         query._path = self._path + (item, ) # update path to include item
         return query # return back query so we can chain getattr calls like key1.key2.key3
+    
+    def _create_query(self, comparison_func):
+        """ Wrapper around common method of creating a query from a specific comparison
+        """
+        def query(value):
+            try:
+                for key in self._path:
+                    value = value[key]
+            except (KeyError, TypeError):
+                return False
+            else:
+                return comparison_func(value)
+        
+        return query
 
     def __eq__(self, rhs):
         """ get a function to call on every document using the provided path
         """
-        def query(value):
-            try:
-                for key in self._path:
-                    value = value[key]
-            except (KeyError, TypeError):
-                return False
-            else:
-                return value == rhs
-        return query
+        # def query(value):
+        #     try:
+        #         for key in self._path:
+        #             value = value[key]
+        #     except (KeyError, TypeError):
+        #         return False
+        #     else:
+        #         return value == rhs
+        # return query
 
-
+        return self._create_query( lambda value : value == rhs)
+    
     def __lt__(self, rhs):
         """ compare against < rhs
         """
-        def query(value):
-            try:
-                for key in self._path:
-                    value = value[key]
-            except (KeyError, TypeError):
-                return False
-            else:
-                return value < rhs
-        return query
+        # def query(value):
+        #     try:
+        #         for key in self._path:
+        #             value = value[key]
+        #     except (KeyError, TypeError):
+        #         return False
+        #     else:
+        #         return value < rhs
+        # return query
+        return self._create_query( lambda value : value < rhs)
--- tests/test_ddb.py
+++ tests/test_ddb.py
@@ -29,7 +29,7 @@ def test_all():
     assert documents == table.all()
 
 
-def test_search():
+def test_search_eq():
     db = Ddb.Database("data.json")
     table = db.table("Words")
     documents = [
@@ -52,5 +52,19 @@ def test_search():
     Word = Ddb.Query()
     assert expected_fruits == table.search(Word.type == "fruit")
     assert expected_vegtables == table.search(Word.type == "vegtable")
-    # assert expected_fruits == words
+    assert [] == table.search(Word.flavor == "spicy")
 
+def test_search_comparisons():
+    db = Ddb.Database("data.json")
+    table = db.table("Scores")
+    documents = {
+        "me" : {"name" : "Galaga", "score": 100, "player": "me"},
+        "m3" :{"name" : "Galaga", "score": 1337, "player": "m3"},
+        "bomberwoman" : {"name" : "Bomberman", "score": 8008135, "player": "bomberwoman"},
+        "hackz" :{"name" : "Bomberman", "score": 803834342, "player": "hackz"}
+    }
+    for doc in documents.values():
+        table.insert(doc)
+    Entry = Ddb.Query()
+    assert documents["me"] == table.search(Entry.score < 1000)[0]
+    # assert len(table.search(Entry.score <= 1337 )) == 2
