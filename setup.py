513a6c4bc898f735da685601e63f5da8b0cf25aa
Dustin Mendoza <penguindustin@gmail.com>
2019-09-24 18:30:02 -0700

    updated changelog and version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ddb",
-    version="0.0.1",
+    version="0.2.0",
     packages=find_packages(exclude=['tests']),
     # metadata for upload to PyPI
     author="Dustin Mendoza",
